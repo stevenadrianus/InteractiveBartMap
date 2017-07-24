dec3d3fcc330b34aea95a1c4f30b6333b3f6a2ed
Steven Adrianus <stevenadrianus@Stevens-MacBook-Pro.local>
2017-07-24 00:28:38 -0700

    Initial commit, maps working

---
+++ script/slideout_setup.js
@@ -0,0 +1,35 @@
+var slideout = new Slideout({
+'panel': document.getElementById('panel'),
+'menu': document.getElementById('menu'),
+'padding': 256,
+'tolerance': 70
+});
+
+// Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function() {
+slideout.toggle();
+});
+
+var fixed = document.querySelector('.fixed-header');
+
+slideout.on('translate', function(translated) {
+  fixed.style.transform = 'translateX(' + translated + 'px)';
+});
+
+slideout.on('beforeopen', function () {
+  fixed.style.transition = 'transform 300ms ease';
+  fixed.style.transform = 'translateX(256px)';
+});
+
+slideout.on('beforeclose', function () {
+  fixed.style.transition = 'transform 300ms ease';
+  fixed.style.transform = 'translateX(0px)';
+});
+
+slideout.on('open', function () {
+  fixed.style.transition = '';
+});
+
+slideout.on('close', function () {
+  fixed.style.transition = '';
+});
